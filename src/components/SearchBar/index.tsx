d73ee22c9850b9a613dfb30e7dba61acae4cc727
gaabssantos <gabriel.oliveira20500@gmail.com>
2025-01-17 20:31:15 -0300

    feat: Criado componente de barra de pesquisa

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,14 @@
+import './styles.scss';
+
+import Search from '../../assets/icons/Search.svg';
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="O que você está buscando?" />
+      <img src={Search} alt="lupa-de-pesquisa" />
+    </div>
+  );
+};
+
+export default SearchBar;
