bb69d8af9542e307f5eb2f437bb28b0911785319
gaabssantos <gabriel.oliveira20500@gmail.com>
2025-01-18 09:49:46 -0300

    assets: Criado componente para Icones

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,10 +3,10 @@ import './styles.scss';
 import Logo from '../../assets/Logo.svg';
 import SearchBar from '../SearchBar';
 
-import Group from '../../assets/icons/Group.png';
-import Favorite from '../../assets/icons/Favorite.svg';
-import User from '../../assets/icons/User.svg';
-import Cart from '../../assets/icons/Cart.svg';
+import FavoritesIcon from '../../icons/FavoritesIcon';
+import UsersIcon from '../../icons/UsersIcon';
+import CartIcon from '../../icons/CartIcon';
+import GroupIcon from '../../icons/GroupIcon';
 
 const NavBar = () => {
   return (
@@ -14,10 +14,10 @@ const NavBar = () => {
       <img src={Logo} alt="logo" />
       <SearchBar />
       <div className="nav-actions">
-        <img src={Group} alt="grupo" />
-        <img src={Favorite} alt="favoritos" />
-        <img src={User} alt="usuário" />
-        <img src={Cart} alt="carrinho" />
+        <GroupIcon />
+        <FavoritesIcon />
+        <UsersIcon />
+        <CartIcon />
       </div>
     </header>
   );
