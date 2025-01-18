7a416d951f2ca25bfd01553b67794c3dec28b7e9
gaabssantos <gabriel.oliveira20500@gmail.com>
2025-01-18 13:10:23 -0300

    feat: Criado Card de Produto

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,20 @@
+import './styles.scss';
+
+import ProductImage from '../../assets/ProductImage.svg';
+import Button from '../Button';
+
+const ProductCard = () => {
+  return (
+    <div className="product-card">
+      <img src={ProductImage} alt="imagem-do-produto" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p id="old-price">R$ 30,90</p>
+      <p id="current-price">R$ 28,90</p>
+      <p id="parcel">ou 2x de R$ 49,95 sem juros</p>
+      <p id="shipping">Frete grátis</p>
+      <Button type="purple">Comprar</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
