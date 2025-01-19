dc36243cb27cc94b40e843129f9a3ef3baf4a978
gaabssantos <gabriel.oliveira20500@gmail.com>
2025-01-19 17:23:51 -0300

    feat: Criado o carrossel

--- src/containers/RelatedProducts/index.tsx
+++ src/containers/RelatedProducts/index.tsx
@@ -1,25 +1,75 @@
+import { useEffect, useState } from 'react';
 import ProductCard from '../../components/ProductCard';
 import RelatedProductsCategory from '../../components/RelatedProductsCategory';
 import Subtitle from '../../components/Subtitle';
 import ArrowIcon from '../../icons/ArrowIcon';
 import './styles.scss';
+import { numberFormat } from '../../utils/NumberFormat';
+import { IProducts } from '../../interface/Products';
+
+interface IProductsData {
+  success: boolean;
+  products: IProducts[];
+}
 
 type RelatedProductsProps = {
   isFirstSection?: boolean;
 };
 
 const RelatedProducts = ({ isFirstSection }: RelatedProductsProps) => {
+  const [products, setProducts] = useState<IProducts[]>();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      fetch('/data/products.json').then(async (res) => {
+        res.json().then((data: IProductsData) => {
+          setProducts(data.products);
+        });
+      });
+    };
+
+    fetchProducts();
+  });
+
+  const goToPrevious = () => {
+    if (products) {
+      setCurrentIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 2 : products?.length - 2,
+      );
+    }
+  };
+
+  const goToNext = () => {
+    if (products) {
+      setCurrentIndex((prevIndex) =>
+        prevIndex < products.length - 4 ? prevIndex + 4 : 0,
+      );
+    }
+  };
+
   return (
     <section className="related-products">
       <Subtitle>Produtos relacionados</Subtitle>
       {!isFirstSection ? <p>Ver todos</p> : <RelatedProductsCategory />}
       <div className="products">
-        <ArrowIcon arrow="left" />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ArrowIcon arrow="right" />
+        <ArrowIcon arrow="left" onClick={() => goToPrevious()} />
+        <div className="carousel-track">
+          {products
+            ?.slice(currentIndex, currentIndex + 4)
+            .map((product) => (
+              <ProductCard
+                productName={product.productName}
+                oldPrice={numberFormat(product.price / 100 + 1000)}
+                parcelPrice={numberFormat(product.price / 100 / 2)}
+                price={product.price}
+                photo={product.photo}
+                descriptionShort={product.descriptionShort}
+                key={product.productName}
+              />
+            ))}
+        </div>
+        <ArrowIcon arrow="right" onClick={() => goToNext()} />
       </div>
     </section>
   );
